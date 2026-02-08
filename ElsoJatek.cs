516ddad9f948bc984cbd65ceb5250de743962872
Huszti Tamás <huszti.tamas@petrik.hu>
2026-02-08 13:05:00 +0100

    Kártyák értékei

--- ElsoJatek.cs
+++ ElsoJatek.cs
@@ -7,10 +7,24 @@ namespace Bűnbarlang
 {
     internal class ElsoJatek
     {
+        private int jatekosPont;
+        private int gepPont;
+
+        public ElsoJatek(int jatekosPont, int gepPont)
+        {
+            this.jatekosPont = jatekosPont;
+            this.gepPont = gepPont;
+            jatekosPont = 0;
+            gepPont = 0;
+        }
+
+        public int JatekosPont { get => jatekosPont; set => jatekosPont = value; }
+        public int GepPont { get => gepPont; set => gepPont = value; }
+
         public void Start()
         {
-            Console.WriteLine("a jatek elindult");
-            Console.ReadKey();
+            Random rnd = new Random();
+
         }
     }
 }
---
+++ KartyaErtek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bűnbarlang
+{
+    internal enum KartyaErtek
+    {
+        Kettő = 2,
+        Három = 3,
+        Négy = 4,
+        Öt = 5,
+        Hat = 6,
+        Hét = 7,
+        Nyolc = 8,
+        Kilenc = 9,
+        Tíz = 10,
+        Jumbó = 10,
+        Dáma = 10,
+        Király = 10,
+        Ász = 11
+    }
+}
