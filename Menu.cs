d69417da4f5c83b37b6f72053483036f93d7bcf8
Huszti Tamás <huszti.tamas@petrik.hu>
2026-02-08 12:33:44 +0100

    Menü

---
+++ Menu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bűnbarlang
+{
+    internal class Menu
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Game()
+    {
+        bool fut = true;
+        while (fut)
+        {
+            Console.Clear();
+            Console.WriteLine("Bűnbarlang\n");
+            Console.WriteLine("1. Első játék");
+            Console.WriteLine("2. Második játék");
+            Console.WriteLine("0. Kilépés");
+            Console.WriteLine("Választás: ");
+            string valasztas = Console.ReadLine();
+            if (valasztas == "1")
+            {
+                Console.WriteLine("1es jatek");
+            }
+            else if (valasztas == "2")
+            {
+                Console.WriteLine("2es jatek");
+            }
+            else if (valasztas == "0")
+            {
+                fut = false;
+            }
+            else
+            {
+                Console.WriteLine("Nem létezik ilyen választás!");
+                Console.ReadKey();
+            }
+        }
+    }
+    static void Main()
+    {
+        Game();
+    }
+}
